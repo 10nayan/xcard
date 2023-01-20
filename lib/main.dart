2fa7581578352afcda49a8a6b2e9c781a1bbecc4
10nayan <nayan.patowary@ambibuzz.com>
2023-01-20 21:51:44 +0530

    new functionality added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+final Uri _url = Uri.parse('tel:+918486890785');
 
 void main() {
   runApp(const MyApp());
@@ -13,45 +16,61 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.lightBlueAccent[400],
-          title: const Text("Xcard"),
+          title: const Text("Liku"),
         ),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               const Text(
-                "Nayan Jyoti Patowary",
+                "Likumani Deka",
                 textAlign: TextAlign.center,
                 style: TextStyle(
-                    color: Colors.black45, fontSize: 30, fontFamily: 'Lobster'),
+                  color: Colors.black45,
+                  fontSize: 36,
+                  fontFamily: 'Lobster',
+                ),
               ),
               const SizedBox(height: 10),
               const CircleAvatar(
                 radius: 70,
-                backgroundImage: AssetImage("images/nayan2.png"),
+                backgroundImage: AssetImage("images/liku3.jpg"),
               ),
               const SizedBox(height: 10),
               const Text(
-                "Mobile App Developer",
+                "BSc Nursing Student",
                 textAlign: TextAlign.center,
                 style: TextStyle(
                     color: Colors.black45,
-                    fontSize: 20,
+                    fontSize: 22,
                     fontWeight: FontWeight.bold),
               ),
               Container(
                 margin: const EdgeInsets.symmetric(
-                    vertical: 10.0, horizontal: 50.0),
+                    vertical: 10.0, horizontal: 40.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5),
+                  color: Colors.white70,
+                ),
                 padding: const EdgeInsets.all(8.0),
-                color: Colors.white,
                 alignment: Alignment.center,
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.start,
-                  children: const [
-                    Icon(Icons.phone),
-                    SizedBox(width: 10),
-                    Text(
-                      "+91 8486890785",
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        _onSearchButtonPressed();
+                      },
+                      tooltip: 'Increase volume by 10',
+                      icon: const Icon(
+                        Icons.phone,
+                        color: Colors.blue,
+                        size: 30,
+                      ),
+                    ),
+                    const SizedBox(width: 10),
+                    const Text(
+                      "+91 9365632699",
                       textAlign: TextAlign.center,
                       style: TextStyle(color: Colors.black45, fontSize: 20),
                     )
@@ -60,17 +79,24 @@ class MyApp extends StatelessWidget {
               ),
               Container(
                 margin: const EdgeInsets.symmetric(
-                    vertical: 10.0, horizontal: 50.0),
+                    vertical: 10.0, horizontal: 40.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5),
+                  color: Colors.white70,
+                ),
                 padding: const EdgeInsets.all(8.0),
-                color: Colors.white,
                 alignment: Alignment.center,
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.start,
                   children: const [
-                    Icon(Icons.mail),
+                    Icon(
+                      Icons.mail,
+                      color: Colors.blue,
+                      size: 30,
+                    ),
                     SizedBox(width: 10),
                     Text(
-                      "nayan.h4.aec@gmail.com",
+                      "likumanideka0@gmail.com",
                       textAlign: TextAlign.center,
                       style: TextStyle(color: Colors.black45, fontSize: 20),
                     )
@@ -84,4 +110,14 @@ class MyApp extends StatelessWidget {
       ),
     );
   }
+}
+
+Future<void> _launchUrl() async {
+  if (!await launchUrl(_url)) {
+    throw 'Could not launch $_url';
+  }
+}
+
+void _onSearchButtonPressed() {
+  print("search button clicked");
 }
